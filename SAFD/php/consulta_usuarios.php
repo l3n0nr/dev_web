c0c9533ece7bf32201d742b1db415ff73c6b6b6f
lenonr <lenonrmsouza@gmail.com>
2016-11-18 09:54:40 -0200

    realizando consulta tabela usuarios

--- SAFD/php/consulta_usuarios.php
+++ SAFD/php/consulta_usuarios.php
@@ -1,46 +1,40 @@
 <?php
 
-  class consulta_usuarios
-  {
-    function consulta()
-    {
-      $servidor = 'localhost';
-      $usuario =  'root';
-      $senha  = '';
-      $banco = 'safd';
+function consulta() {
+    $servidor = 'localhost';
+    $usuario = 'root';
+    $senha = '';
+    $banco = 'safd';
 
-      $conexao = mysql_connect($servidor, $usuario, $senha, $banco);
+    $conexao = mysql_connect($servidor, $usuario, $senha, $banco);
 
-      if (!$link = mysql_connect($servidor, $usuario, $senha))
-      {
-          echo 'Não foi possível conectar ao mysql';
-          exit;
-      }
+    if (!$link = mysql_connect($servidor, $usuario, $senha)) {
+        echo 'Não foi possível conectar ao mysql';
+        exit;
+    }
 
-      if (!mysql_select_db($banco, $link)) {
-          echo 'Não foi possível selecionar o banco de dados';
-          exit;
-      }
+    if (!mysql_select_db($banco, $link)) {
+        echo 'Não foi possível selecionar o banco de dados';
+        exit;
+    }
 
-      $sql    = 'SELECT * FROM usuario';
-      $result = mysql_query($sql, $link);
+//      $sql    = 'SELECT * FROM usuario';
+    $sql = 'SELECT * FROM funcionario';
+    $result = mysql_query($sql, $link);
 
-      if (!$result)
-      {
-          echo "Erro do banco de dados, não foi possível consultar o banco d dados\n";
-          echo "Erro MySQL: " . mysql_error();
-          exit;
-      }
+    if (!$result) {
+        echo "Erro do banco de dados, não foi possível consultar o banco d dados\n";
+        echo "Erro MySQL: " . mysql_error();
+        exit;
+    }
 
-      while ($row = mysql_fetch_assoc($result))
-      {
-          $visualizar =
-            "<tr>
-                <td>".$row['id_funcionario']."</td>"."
-                <td>".$row['siape_funcionario']."</td>"."
-                </td>"."
+    while ($row = mysql_fetch_assoc($result)) {
+        $visualizar = "<tr>
+                <td>" . $row['id_funcionario'] . "</td>" . "
+                <td>" . $row['siape_funcionario'] . "</td>" . "
+                </td>" . "
             </tr>";
-          
+
 //            "<tr>
 //            <td>".$row['id_setor']."</td>"."
 //            <td>".$row['estado_usuario']."</td>"."
@@ -52,16 +46,15 @@
 //            </td>"."
 //            </tr>";
 
-          /*
-            $usuarios = consulta_todos_usuarios();
-            echo $usuarios;
-          */
+        /*
+          $usuarios = consulta_todos_usuarios();
+          echo $usuarios;
+         */
 
-          echo $visualizar;
-      }
-
-      mysql_free_result($result);
-
+        echo $visualizar;
     }
-  }
+
+    mysql_free_result($result);
+}
+
 ?>
