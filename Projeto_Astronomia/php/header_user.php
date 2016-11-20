257d4563337be2a43286addb52dd91420a48e0cd
lenonr <lenonrmsouza@gmail.com>
2016-11-20 15:32:09 -0200

    Background image index

--- Projeto_Astronomia/php/header_user.php
+++ Projeto_Astronomia/php/header_user.php
@@ -1,4 +1,4 @@
-<a href="system_user.php" class="logo">
+<a href="system.php" class="logo">
     <!-- logo for regular state and mobile devices -->
     <span class="logo-mini">P_A</span>
     <span class="logo-lg">Projeto_Astronomia</span>
@@ -6,8 +6,7 @@
 
 <!-- Header Navbar: style can be found in header.less -->
 <nav class="navbar navbar-static-top" role="navigation">
-
-    <div class="navbar-custom-menu">
+    <div class="">
         <ul class="nav navbar-nav">
             <a href="#" class="sidebar-toggle" data-toggle="offcanvas" role="button">
                 <span class="sr-only"></span>
@@ -16,8 +15,27 @@
     </div>
     <form class="navbar-form navbar-left" role="search">
         <div class="form-group">
-            <input class="form-control" id="navbar-search-input" placeholder="Digite algo Aqui" type="text">
+            <input class="form-control" id="navbar-search-input" placeholder="Digite algum termo aqui" type="text">
         </div>
-    </form>       
+    </form>
+    <ul class="nav navbar-nav">
+        <li><a href="#"> Sistema Solar</a></li>
+        <li><a href="#"> ExoPlanetas</a></li>
+        <li><a href="#"> Rovers</a></li>
+        <li><a href="#"> Sondas</a></li>
+        <li><a href="#"> Satélites</a></li>
+        <li class="dropdown">
+<!--            <a href="#" class="dropdown-toggle" data-toggle="dropdown">Dropdown <span class="caret"></span></a>
+            <ul class="dropdown-menu" role="menu">
+                <li><a href="#">Action</a></li>
+                <li><a href="#">Another action</a></li>
+                <li><a href="#">Something else here</a></li>
+                <li class="divider"></li>
+                <li><a href="#">Separated link</a></li>
+                <li class="divider"></li>
+                <li><a href="#">One more separated link</a></li>
+            </ul>-->
+        </li>
+    </ul>        
 </nav>
 
