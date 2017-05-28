95eebb9b07384dc6e267c254ef09bb02f8bc0954
lenonr <lenonrmsouza@gmail.com>
2017-05-28 16:21:43 -0300

    ca

--- SAFD/php/cadastra_usuarios.php
+++ SAFD/php/cadastra_usuarios.php
@@ -1,43 +1,58 @@
 <?php
-    //servidor
-        $servidor = 'localhost';
-        $usuario = 'lenonr';
-        $senha = ' ';
-        $banco = 'safd';
-        
-//     //recebendo dados do formulario
-//         $nome   = $_POST["nome"];
-//         $email  = $_POST["email"];
-//         $siape  = $_POST["siape"];
-//         $setor  = $_POST["setor"];
-//         $funcao = $_POST["funcao"];
-//         $login  = $_POST["login"];
-//         $senha  = $_POST["senha"];
 
-    //conectando banco
-        $con = new mysqli($servidor, $usuario, $senha, $banco);
-    
-    //verificando conexao
-        if ($con->connect_error) 
-        {
-            die("Connection failed: " . $con->connect_error);
-        }
-        else
-        {
-            echo "Conectou no servidor!!";                 
-        }
-                
-        if (!mysql_select_db($banco, $con)) 
-        {
-            echo 'Não foi possível selecionar o banco de dados';
-//             exit;
-            
-            echo "Mostra erro banco..";
-        }
-        else
-        {        
-            echo "Conectou no banco de dados! \n";
-        } 
+
+
+//     //servidor
+//         $servidor = 'localhost';
+//         $usuario = 'lenonr';
+//         $senha = ' ';
+//         $banco = 'safd';
+//         
+// //     //recebendo dados do formulario
+// //         $nome   = $_POST["nome"];
+// //         $email  = $_POST["email"];
+// //         $siape  = $_POST["siape"];
+// //         $setor  = $_POST["setor"];
+// //         $funcao = $_POST["funcao"];
+// //         $login  = $_POST["login"];
+// //         $senha  = $_POST["senha"];
+// 
+//     //conectando banco
+//         $con = new mysqli($servidor, $usuario, $senha, $banco);
+//     
+//     //verificando conexao
+//         if ($con->connect_error) 
+//         {
+//             die("Connection failed: " . $con->connect_error);
+//         }
+//         else
+//         {
+//             echo "Conectou no servidor!!";                 
+//         }
+//                 
+//         if (!mysql_select_db($banco, $con)) 
+//         {
+//             echo 'Não foi possível selecionar o banco de dados';
+// //             exit;
+//             
+//             echo "Mostra erro banco..";
+//         }
+//         else
+//         {        
+//             echo "Conectou no banco de dados! \n";
+//             
+//             $query = 
+//                 "INSERT INTO funcionario (nome_funcionario, email_funcionario, siape_funcionario)
+//                     VALUES ('$nome', '$email', '$siape')"
+// //                 INSERT INTO setor (nome_setor)
+// //                     VALUES('$setor')
+// //                 INSERT INTO funcao (descricao_funcao)   
+// //                     VALUES('$funcao')"
+// 
+//              mysql_query($query,$con);
+//              
+//              include 'inserir_usuarios.php';
+//         } 
         
         
 //     echo "Conexao com mysql_connect";
--- SAFD/php/consulta_usuarios.php
+++ SAFD/php/consulta_usuarios.php
@@ -1,5 +1,46 @@
 <?php
- //servidor
+//conectando servidor
+    define( 'MYSQL_HOST', 'localhost' );
+    define( 'MYSQL_USER', 'lenonr' );
+    define( 'MYSQL_PASSWORD', ' ' );
+    define( 'MYSQL_DB_NAME', 'safd' );
+
+//tentando realizar conexao
+    try
+    {
+        //realizando conexao atraves do objeto    
+        $PDO = new PDO( 'mysql:host=' . MYSQL_HOST . ';dbname=' . MYSQL_DB_NAME, MYSQL_USER, MYSQL_PASSWORD );
+    }
+    catch ( PDOException $e )
+    {
+        //mostrando mensagem de erro
+        echo 'Erro ao conectar com o MySQL: ' . $e->getMessage();
+    }
+    
+//realizando consulta no banco de dados
+    $sql = "SELECT * FROM funcionario";
+    $result = $PDO->query( $sql );
+    $rows = $result->fetchAll( PDO::FETCH_ASSOC );
+    
+    while ($rows == TRUE) 
+    {
+        $visualizar = 
+            "<tr>
+                <td>".$rows['siape_funcionario']."</td>"."                                   
+                <td>".$rows['nome_funcionario']."</td>"."            
+                <td>".$rows['email_funcionario']."</td>"."
+            </tr>";
+
+        echo $visualizar;
+    }
+    
+
+//     print_r( $rows );
+    
+    
+
+
+/* //servidor
         $servidor = 'localhost';
         $usuario = 'lenonr';
         $senha = ' ';
@@ -15,30 +56,33 @@
         }
         else
         {
-            //echo "Conectou no servidor!!";    
+            echo "Conectou no servidor!!";    
             $sql_usuario    = 'SELECT * FROM funcionario';
-            $result = mysql_query($sql_usuario, $con);
+            $result = mysqli_query($sql_usuario, $con);
 
-            if (!$result) 
+//             if (!$result) 
+//             {
+//                 echo "Erro do banco de dados, não foi possível consultar o banco d dados\n";
+//                 echo "Erro MySQL: " . mysql_error();
+//                 exit;
+//             }
+
+            while ($row = mysqli_fetch_assoc($result)) 
             {
-                echo "Erro do banco de dados, não foi possível consultar o banco d dados\n";
-                echo "Erro MySQL: " . mysql_error();
-                exit;
-            }
-
-            while ($row = mysql_fetch_assoc($result)) {
                 $visualizar = 
                     "<tr>
-                    <td>".$row['siape_funcionario']."</td>"."                                   
-                    <td>".$row['nome_funcionario']."</td>"."            
-                    <td>".$row['email_funcionario']."</td>"."
+                        <td>".$row['siape_funcionario']."</td>"."                                   
+                        <td>".$row['nome_funcionario']."</td>"."            
+                        <td>".$row['email_funcionario']."</td>"."
                     </tr>";
 
                 echo $visualizar;
             }
 
             mysql_free_result($result);
-        }                
+            
+            echo "teste";
+        }           */     
 
 // // // // // // // // // // // // // // // 
 
