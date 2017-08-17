87468e10bcf153a9cbd2000165b908ab7caefa29
lenonr <lenonrmsouza@gmail.com>
2017-08-17 17:41:09 -0300

    tabela administrador adicionada

--- SAFD/database/base.sql
+++ SAFD/database/base.sql
@@ -1,5 +1,5 @@
 CREATE TABLE IF NOT EXISTS funcao  (
-    id_funcao int(5) NOT NULL PRIMARY KEY,
+    id_funcao int(5) NOT NULL PRIMARY KEY AUTO_INCREMENT,
 	descricao_funcao VARCHAR(40) NOT NULL); 
 
 CREATE TABLE IF NOT EXISTS funcionario(
@@ -8,18 +8,18 @@ CREATE TABLE IF NOT EXISTS funcionario(
         email_funcionario VARCHAR(40) NOT NULL);      	
 
 CREATE TABLE IF NOT EXISTS usuario(
-    id_setor int(10) NOT NULL,
+    id_setor int(10) NOT NULL AUTO_INCREMENT,
 	estado_usuario binary NOT NULL, 
 	login_usuario VARCHAR(50) NOT NULL,
 	senha_usuario VARCHAR(50) NOT NULL);		
 
 CREATE TABLE IF NOT EXISTS usuario_cadastrado(
-    numero_portaria int(5) NOT NULL,
+    numero_portaria int(5) NOT NULL ,
 	datainicio date NOT NULL,
 	datafim date NOT NULL);
 		
 CREATE TABLE IF NOT EXISTS setor(
-    id_setor int(10) NOT NULL PRIMARY KEY,
+    id_setor int(10) NOT NULL PRIMARY KEY AUTO_INCREMENT,
 	nome_setor VARCHAR(40) NOT NULL);
 		
 CREATE TABLE IF NOT EXISTS funcao_funcionario(
@@ -27,13 +27,13 @@ CREATE TABLE IF NOT EXISTS funcao_funcionario(
 	data_funcaofuncionario DATE);
 
 CREATE TABLE IF NOT EXISTS solicitacao(
-    id_solicitacao int(10) NOT NULL PRIMARY KEY,
+    id_solicitacao int(10) NOT NULL PRIMARY KEY AUTO_INCREMENT,
 	justificativa_solicitacao VARCHAR(40) NOT NULL,
 	data_solicitacao VARCHAR(40) NOT NULL,
 	numeroprotocolo_solicitacao VARCHAR(40) NOT NULL);
 
 CREATE TABLE IF NOT EXISTS status_solicitacao(
-    id_statussolicitacao int(10) NOT NULL PRIMARY KEY,
+    id_statussolicitacao int(10) NOT NULL PRIMARY KEY AUTO_INCREMENT,
 	nome_statussolicitacao VARCHAR(40) NOT NULL);
 
 CREATE TABLE IF NOT EXISTS unidade(
@@ -49,7 +49,7 @@ CREATE TABLE IF NOT EXISTS objeto(
 	nome_objeto VARCHAR(40) NOT NULL);
 
 CREATE TABLE IF NOT EXISTS avaliacao_dpdi(
-    id_avaliacaodpdi int(10) NOT NULL PRIMARY KEY,
+    id_avaliacaodpdi int(10) NOT NULL PRIMARY KEY AUTO_INCREMENT,
 	comentarios_avaliacaodpdi VARCHAR(40) NOT NULL,
 	planejamentoexercicio_avaliacaodpdi VARCHAR(40) NOT NULL);
 
@@ -58,7 +58,7 @@ CREATE TABLE IF NOT EXISTS unidade_gestora(
 	nome_unidadegestora VARCHAR(40) NOT NULL);
 
 CREATE TABLE IF NOT EXISTS avaliacao_dad(
-    id_avaliacaodad int(10) NOT NULL PRIMARY KEY,
+    id_avaliacaodad int(10) NOT NULL PRIMARY KEY AUTO_INCREMENT,
 	recursos_avaliacaodad VARCHAR(400) NOT NULL,
 	comentarios_avaliacaodad VARCHAR(400) NOT NULL,
 	valorestimadodespesa_avaliacaodad int(40) NOT NULL);
@@ -68,9 +68,15 @@ CREATE TABLE IF NOT EXISTS tipo_despesa(
 	nome_tipodespesa VARCHAR(40) NOT NULL);
 
 CREATE TABLE IF NOT EXISTS licitacao(
-    id_licitacao int(10) NOT NULL PRIMARY KEY,
+    id_licitacao int(10) NOT NULL PRIMARY KEY AUTO_INCREMENT,
 	comentarios_licitacao VARCHAR(40) NOT NULL);
 
 CREATE TABLE IF NOT EXISTS status_licitacao(
-    id_statuslicitacao int(10) NOT NULL PRIMARY KEY,
+    id_statuslicitacao int(10) NOT NULL PRIMARY KEY AUTO_INCREMENT,
 	nome_statuslicitacao VARCHAR(40) NOT NULL);
+	
+CREATE TABLE IF NOT EXISTS administrador(
+    id_administrador int(10) NOT NULL PRIMARY KEY AUTO_INCREMENT,
+        nome_administrador VARCHAR(40) NOT NULL,
+        senha_administrador VARCHAR(40) NOT NULL,
+        habilitado_administrador BINARY NOT NULL);
