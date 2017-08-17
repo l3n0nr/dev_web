7354e7f0078b6b3cfe5af4669c6761685d206b28
lenonr <lenonrmsouza@gmail.com>
2017-08-17 14:56:38 -0300

    codigo melhorado - siape auto increment

--- SAFD/php/cadastra_usuarios.php
+++ SAFD/php/cadastra_usuarios.php
@@ -3,14 +3,14 @@
     include_once "conexao.php";
 
     //recebendo dados do formulario
-    $siape  = $_POST['siape_funcionario'];
+//     $siape  = $_POST['siape_funcionario'];
     $nome   = $_POST['nome_funcionario'];
     $email  = $_POST['email_funcionario'];
     
     //mostrando dados inseridos
 //     echo "Dados - Siape:" . $siape . ", Nome:" . $nome . ", Email:" . $email. ".<hr>";    
    
-// INSERÇÃO
+// INSERÇÃO - campo siape_funcionario irá ser usado futuramente como campo de preenchimento manual
     $sql  = 'INSERT INTO funcionario(siape_funcionario, nome_funcionario, email_funcionario) ';
     $sql .= 'VALUES(:siape, :nome, :email)';
     
