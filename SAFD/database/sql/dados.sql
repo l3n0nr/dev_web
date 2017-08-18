ab8b1074e0f292e95980ae342d0b96e60b0291b0
lenonr <lenonrmsouza@gmail.com>
2017-08-18 15:01:00 -0300

    Inserção de dados adicionada

--- SAFD/database/sql/dados.sql
+++ SAFD/database/sql/dados.sql
@@ -1,14 +1,31 @@
+-- -- NÃO É NECESSÁRIO, REMOVER!
 INSERT INTO administrador(nome_administrador, senha_administrador, habilitado_administrador)
         VALUES("admin", "admin", "0");
         
+-- -- NÃO É NECESSÁRIO, REMOVER!        
 INSERT INTO administrador(nome_administrador, senha_administrador, habilitado_administrador)
         VALUES("lenonr", "lenonr", "1"); 
+
+-- -- ADICIONANDO USUARIO        
+INSERT INTO setor(nome_setor)
+    VALUES("administração");
+    
+INSERT INTO usuario(id_setor, estado_usuario, login_usuario, senha_usuario)
+    VALUES(1, 1, "admin", "admin");
+    
+INSERT INTO usuario(id_setor, estado_usuario, login_usuario, senha_usuario)
+    VALUES(1, 1, "lenonr", "lenonr");
+    
+INSERT INTO usuario(id_setor, estado_usuario, login_usuario, senha_usuario)
+    VALUES(1, 0, "teste", "teste");
         
 INSERT INTO funcao(descricao_funcao)        
     VALUES("Administrador");
     
 INSERT INTO funcao(descricao_funcao)        
     VALUES("Padrao");
+    
+-- -- -- -- -- -- -- --     
     
     
     
