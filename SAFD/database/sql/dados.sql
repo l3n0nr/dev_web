305912158dcf2ec6bbce72e108f2418a2952b8fe
lenonr <lenonrmsouza@gmail.com>
2017-08-18 14:44:27 -0300

    dados de inserção da tabela funcao adicionados

--- SAFD/database/sql/dados.sql
+++ SAFD/database/sql/dados.sql
@@ -2,6 +2,17 @@ INSERT INTO administrador(nome_administrador, senha_administrador, habilitado_ad
         VALUES("admin", "admin", "0");
         
 INSERT INTO administrador(nome_administrador, senha_administrador, habilitado_administrador)
-        VALUES("lenonr", "lenonr", "1");        
+        VALUES("lenonr", "lenonr", "1"); 
+        
+INSERT INTO funcao(descricao_funcao)        
+    VALUES("Administrador");
+    
+INSERT INTO funcao(descricao_funcao)        
+    VALUES("Padrao");
+    
+    
+    
+
+
         
         
