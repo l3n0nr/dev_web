d3b3f8487f07afcebe097cf733cf0977b5104dbf
lenonr <lenonrmsouza@gmail.com>
2017-08-17 22:09:30 -0300

    função funcionando, [ERRO] quando usuario nao existe

--- SAFD/php/valida_usuario.php
+++ SAFD/php/valida_usuario.php
@@ -3,14 +3,13 @@
   $senha = $_POST["senha"];
 
 //   echo "Login: " .$login. " ,Senha: ".$senha.".";
-  echo "Entrada: " . $login."|".$senha;
+//   echo "Entrada: " . $login."|".$senha;
   
   //incluindo arquivo conexao
     include_once 'conexao.php';
     
 //realizando consulta no banco de dados
-//     $consulta = "SELECT nome_administrador FROM administrador WHERE nome_administrador='$login' AND senha_administrador='$senha' AND habilitado_administrador != '0' ";
-    $consulta = "SELECT nome_administrador, senha_administrador FROM administrador WHERE nome_administrador='$login' AND senha_administrador='$senha' ";        
+    $consulta = "SELECT nome_administrador, senha_administrador FROM administrador WHERE nome_administrador='$login' AND senha_administrador='$senha' AND habilitado_administrador != '0' ";
     
     $result = $db->query($consulta);
     
@@ -31,25 +30,26 @@
                         </tr>";
                         
             #mostrando colunas/linhas
-                echo "<hr>".$visualizar;   
-                                
-            if(mysqli_num_rows($result)>0)	// usuário ok, conclui pedido
-            {                
+//                 echo "<hr>".$visualizar;                                               
+                
+            if(mysqli_num_rows($result)!=0)	 
+            {   
+                echo "<p class=\"erro\">Dados incorretos! Favor verificar seu e-mail e senha e tentar novamente!</p>";                
+                session_start();
+                session_destroy();
+                header("location:index.php");
+            }
+            else
+            {
                 echo "Usuário cadastro, agora você pode acessar o sistema";
                 session_start();
                 session_cache_expire(10);
                 $_SESSION["usuario"]=$login;
                 $_SESSION["senha"]=$senha;
-                header("location:system_admin.php");
-            }
-            else
-            {
-                echo "<p class=\"erro\">Dados incorretos! Favor verificar seu e-mail e senha e tentar novamente!</p>";
-            }
+                header("location:system_admin.php");                
+            }                
+                    
         }
-        
-        
-//     }    
   
 //   if ($login =="admin" && $senha =="admin")
 //   {
