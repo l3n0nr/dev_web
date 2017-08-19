5d83c4984b05e62244bfbac76f2cb5e8921724c9
lenonr <lenonrmsouza@gmail.com>
2017-08-19 10:56:53 -0300

    Insert no banco, adicionado

--- SAFD/database/sql/dados.sql
+++ SAFD/database/sql/dados.sql
@@ -10,6 +10,9 @@ INSERT INTO administrador(nome_administrador, senha_administrador, habilitado_ad
 INSERT INTO setor(nome_setor)
     VALUES("administração");
     
+INSERT INTO setor(nome_setor)
+    VALUES("padrao");
+    
 INSERT INTO usuario(id_setor, estado_usuario, login_usuario, senha_usuario)
     VALUES(1, 1, "admin", "admin");
     
