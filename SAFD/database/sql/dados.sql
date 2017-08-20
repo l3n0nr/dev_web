f80e5df638038993d84e4b7b0f2d941d80295a71
lenonr <lenonrmsouza@gmail.com>
2017-08-20 14:38:11 -0300

    insert modificados

--- SAFD/database/sql/dados.sql
+++ SAFD/database/sql/dados.sql
@@ -17,10 +17,13 @@ INSERT INTO usuario(id_setor, estado_usuario, login_usuario, senha_usuario)
     VALUES(1, 1, "admin", "admin");
     
 INSERT INTO usuario(id_setor, estado_usuario, login_usuario, senha_usuario)
-    VALUES(1, 1, "lenonr", "lenonr");
+    VALUES(2, 1, "lenonr", "lenonr");
     
 INSERT INTO usuario(id_setor, estado_usuario, login_usuario, senha_usuario)
     VALUES(1, 0, "teste", "teste");
+    
+INSERT INTO usuario(id_setor, estado_usuario, login_usuario, senha_usuario)
+    VALUES(2, 0, "teste1", "teste1");
         
 INSERT INTO funcao(descricao_funcao)        
     VALUES("Administrador");
