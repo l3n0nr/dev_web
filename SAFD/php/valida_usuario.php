9be1278bf1ebcc5fa666dcc16288849d0a7e7013
lenonr <lenonrmsouza@gmail.com>
2017-11-07 10:44:52 -0200

    trata usuario desativado

--- SAFD/php/valida_usuario.php
+++ SAFD/php/valida_usuario.php
@@ -1,9 +1,4 @@
 <?php
-/*
-  AÇÕES PENDENTES:
-  -Criar um verificação para analisar o tipo do usuario cadastrado. Se "administrador" entra na parte de administrador do sistema, se usuario "padrao" entra na parte normal de sistema.
-  -Criar posteriormente os niveis de Diretor, Coordenador, Chefe e Outros.
- */
 
 # arquivo conexao
 include 'conexao.php';
@@ -13,43 +8,62 @@ $login = $_POST["login"];
 $senha = $_POST["senha"];
 
 # definindo comando para consulta
-$consulta = "SELECT login_usuario, senha_usuario, estado_usuario, id_funcao FROM usuario WHERE login_usuario = '$login' AND senha_usuario = '$senha' AND estado_usuario=1";
-
-# realiza consulta
-$res = mysqli_query($con, $consulta);
+$consulta = "SELECT login_usuario, senha_usuario, estado_usuario, id_funcao
+             FROM usuario
+             WHERE login_usuario = '$login'
+             AND senha_usuario = '$senha';
+            --  AND estado_usuario=1";
 
 # verifica se usuario foi digitado
 if (isset($login))
 {
-    # verifica usuario
-    if (mysqli_num_rows($res) != 0)
-    { // usuário ok, conclui pedido
-        # inicia sessao
-        session_start();
-        session_cache_expire(10);
+    // verificando estado usuario
+    $estado = "SELECT estado_usuario FROM usuario WHERE login_usuario = '$login'";
 
-        # salva usuario para ser mostrado no menu da pagina
-        $_SESSION["usuario"] = $login;
+    // preparando variavel
+    $valor_estado = mysqli_query($con, $estado);
 
-//        echo $login;
+    // pesquisando na lista - ultimo valor cadastrado
+    while($res = mysqli_fetch_assoc($valor_estado))
+    {
+        // capturando valor - estado usuario
+        $valor_estado = $res['estado_usuario'];
+    }
 
-        // realizando busca pelo ultimo usuario cadastrado
-        $funcionario = "SELECT id_funcao FROM usuario WHERE login_usuario = '$login'";
+    // tratando usuario desativado
+    if ($valor_estado == 0)
+    {
+        # mostra mensagem e redireciona pagina
+        echo ("<script>alert('Por favor, entre em contato com o Administrador do Sistema!'); location.href='index.html';</script>");
+    }
+    else
+    {
+        # realiza consulta
+        $res = mysqli_query($con, $consulta);
 
-        // preparando variavel
-        $result = mysqli_query($con, $funcionario);
-
-        // pesquisando na lista - ultimo valor cadastrado
-        while($res = mysqli_fetch_assoc($result))
+        # verifica usuario
+        if (mysqli_num_rows($res) != 0)
         {
-            $funcao = $res['id_funcao'];
-        }
-//             $consulta_tipo = "SELECT id_funcao FROM usuario WHERE login_usuario = '$login' ";
-//
-////             echo $consulta_tipo;
-//
-//             $res_tipo= mysqli_query($con, $consulta_tipo);
-//
+            # iniciando sessao
+            session_start();
+            session_cache_expire(10);
+
+            # salva usuario para ser mostrado no menu da pagina
+            $_SESSION["usuario"] = $login;
+
+            // realizando busca pelo ultimo usuario cadastrado
+            $funcionario = "SELECT id_funcao FROM usuario WHERE login_usuario = '$login'";
+
+            // preparando variavel
+            $result = mysqli_query($con, $funcionario);
+
+            // pesquisando na lista - ultimo valor cadastrado
+            while($res = mysqli_fetch_assoc($result))
+            {
+                $funcao = $res['id_funcao'];
+            }
+
+            // controle de acesso dos usuarios
             if ($funcao == 1)
             {
                 # redireciona pagina
@@ -74,19 +88,20 @@ if (isset($login))
             {
                 echo "outro";
             }
-    }
-    else
-    {
-//      destruindo sessao
-//	session_destroy();
+        }
+        else
+        {
+            // destruindo sessao
+        	session_destroy();
 
-        //ideal para utilizar depois do sistema estiver funcionar com os usuarios separados - funcoes
-//        limpando sessao
-//	unset ($_SESSION['login']);
-//	unset ($_SESSION['senha']);
+            //ideal para utilizar depois do sistema estiver funcionar com os usuarios separados - funcoes
+            // limpando sessao
+        	unset ($_SESSION['login']);
+        	unset ($_SESSION['senha']);
 
-        # mostra mensagem e redireciona pagina
-        echo ("<script>alert('Usuário ou Senha incorretos!'); location.href='index.html';</script>");
+            # mostra mensagem e redireciona pagina
+            echo ("<script>alert('Usuário ou Senha incorretos!'); location.href='index.html';</script>");
+        }
     }
 }
 ?>
