9c963d5a1f51ddc9d4cb461c8deed0bc4a47f089
lenonr <lenonrmsouza@gmail.com>
2017-11-13 11:26:26 -0200

    titulo menu otimizado

--- SAFD/php/menulat_admin.php
+++ SAFD/php/menulat_admin.php
@@ -9,9 +9,9 @@
               </div>
             </form>-->
         <li class="header">USUÁRIOS</li>
-        <li><a href="p_insere_usuarios.php"><i class="glyphicon glyphicon-plus"></i> <span>Inserir</span></a></li>
-        <li><a href="p_visualiza_usuarios.php"><i class="glyphicon glyphicon-sunglasses"></i> <span>Visualizar</span></a></li>
-        <li><a href="p_edita_usuarios.php"><i class="glyphicon glyphicon-list-alt"></i> <span>Alterar</span></a></li>
-        <li><a href="p_exclui_usuarios.php"><i class="glyphicon glyphicon-trash"></i> <span>Excluir</span></a></li>
+        <li><a href="p_insere_usuarios.php"><i class="glyphicon glyphicon-plus"></i> <span>Inserir usuários</span></a></li>
+        <li><a href="p_visualiza_usuarios.php"><i class="glyphicon glyphicon-sunglasses"></i> <span>Visualizar usuários</span></a></li>
+        <li><a href="p_edita_usuarios.php"><i class="glyphicon glyphicon-list-alt"></i> <span>Alterar usuários</span></a></li>
+        <li><a href="p_exclui_usuarios.php"><i class="glyphicon glyphicon-trash"></i> <span>Excluir usuário</span></a></li>
     </ul>
 </section>
