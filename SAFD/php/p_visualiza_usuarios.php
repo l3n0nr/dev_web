298877ad2a007cc1171110ad10d96e1486b84c40
lenonr <lenonrmsouza@gmail.com>
2017-11-13 11:25:27 -0200

    layout alterado para sidebar-collapse

--- SAFD/php/p_visualiza_usuarios.php
+++ SAFD/php/p_visualiza_usuarios.php
@@ -19,7 +19,8 @@
         <link rel="stylesheet" href="../dist/css/skins/_all-skins.min.css">
     </head>
     <!--<body class="hold-transition skin-green fixed sidebar-mini">-->
-    <body class="hold-transition skin-green layout-fixed sidebar-mini">
+    <!-- <body class="hold-transition skin-green layout-fixed sidebar-mini"> -->
+    <body class="hold-transition skin-green sidebar-collapse sidebar-mini">
         <!-- Site wrapper -->
         <div class="wrapper">
             <header class="main-header" >
