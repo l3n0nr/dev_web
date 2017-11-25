f3b96dac9af85a6ebdbffba2adbb72a186bdcb24
lenonr <lenonrmsouza@gmail.com>
2017-11-25 14:57:33 -0200

    paginas do diretor melhoradas

--- SAFD/php/menulat_diretor.php
+++ SAFD/php/menulat_diretor.php
@@ -15,20 +15,20 @@
         -->
 
         <li class="header">DEMANDAS</li>
-        <li><a href="p_dem_pendentes.php"><i class="fa fa-circle text-red"></i> <span>Demandas Pendentes</span></a></li>
-        <li><a href="p_hist_demandas.php"><i class="fa fa-circle text-green"></i> <span>Historico de Demandas</span></a></li>
+        <li><a href="p_dem_pendentes_diretor.php"><i class="fa fa-circle text-red"></i> <span>Demandas Pendentes</span></a></li>
+        <li><a href="p_hist_demandas_diretor.php"><i class="fa fa-circle text-green"></i> <span>Historico de Demandas</span></a></li>
 
-        <li class="header">DOCUMENTOS</li>
-        <li><a href="form_bens.php"><i class="glyphicon glyphicon-list-alt"></i> <span>Solicitação de Bens</span></a></li>
+        <!-- <li class="header">DOCUMENTOS</li>
+        <li><a href="form_bens.php"><i class="glyphicon glyphicon-list-alt"></i> <span>Solicitação de Bens</span></a></li> -->
 
-        <!-- <li class="header">AVALIAÇÕES</li>
-        <li><a href="form_bens_coord.php"><i class="glyphicon glyphicon-font"></i>
+        <li class="header">AVALIAÇÕES</li>
+        <!-- <li><a href="form_bens_coord.php"><i class="glyphicon glyphicon-font"></i>
                 <span> Coordenador </span></a></li>
         <li><a href="form_bens_dpdi.php"><i class="glyphicon glyphicon-font"></i>
                 <span> DPDI </span></a></li>
         <li><a href="form_bens_dad.php"><i class="glyphicon glyphicon-font"></i>
-                <span> DAD </span></a></li>
+                <span> DAD </span></a></li> -->
         <li><a href="form_bens_dg.php"><i class="glyphicon glyphicon-font"></i>
-                <span> DG </span></a></li> -->
+                <span> DG </span></a></li>
     </ul>
 </section>
