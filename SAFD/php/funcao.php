3805aec491bc75cb467767a5249b82fe0d38480b
lenonr <lenonrmsouza@gmail.com>
2017-11-25 21:22:13 -0200

    funcao tae e docente adicionadas

--- SAFD/php/funcao.php
+++ SAFD/php/funcao.php
@@ -1,5 +1,5 @@
 <option value="0"> Função </option>
 <option value="2"> Diretor </option>
 <option value="3"> Coordenador </option>
-<option value="4"> Chefe </option>
-<option value="5"> Professor </option>
+<option value="4"> TAE </option>
+<option value="5"> Docente </option>
