28c3dd7e762d262907680262262a8d890b2d7451
lenonr <lenonrmsouza@gmail.com>
2017-11-25 21:21:09 -0200

    paginas da funcao tae melhoradas

---
+++ SAFD/php/header_tae.php
@@ -0,0 +1,25 @@
+<?php
+    include 'valida_sessao.php';
+?>
+<a href="system_tae.php" class="logo">
+    <!-- logo for regular state and mobile devices -->
+    <span class="logo-mini">SAFD</span>
+    <span class="logo-lg">SAFD</span>
+</a>
+<!-- Header Navbar: style can be found in header.less -->
+<nav class="navbar navbar-static-top" role="navigation">
+    <a href="#" class="sidebar-toggle" data-toggle="offcanvas" role="button">
+        <span class="sr-only"></span>
+    </a>
+    <div class="navbar-custom-menu">
+        <ul class="nav navbar-nav">
+            <li class="dropdown">
+                <a aria-expanded="false" href="#" class="dropdown-toggle" data-toggle="dropdown"> TAE: <?php echo $_SESSION["usuario"] ?>
+                <span class="caret"></span></a>
+                <ul class="dropdown-menu" role="menu">
+                    <li><a href="../index.html">Sair</a></li>
+                </ul>
+            </li>
+        </ul>
+    </div>
+</nav>
---
+++ SAFD/php/menulat_tae.php
@@ -0,0 +1,17 @@
+<section class="sidebar">
+    <ul class="sidebar-menu">
+        <!--        <form action="busca.php" method="get" class="sidebar-form">
+                    <div class="input-group">
+                        <input name="q" class="form-control" placeholder="Pesquise algo aqui.." type="text">
+                        <span class="input-group-btn">
+                            <button type="submit" name="search" id="search-btn" class="btn btn-flat"><i class="fa fa-search"></i></button>
+                        </span>
+                    </div>
+                </form>-->
+        <li class="header">DEMANDAS</li>
+        <li><a href="p_dem_pendentes_tae.php"><i class="fa fa-circle text-red"></i> <span>Demandas Pendentes</span></a></li>
+        <li><a href="p_hist_demandas_tae.php"><i class="fa fa-circle text-green"></i> <span>Historico de Demandas</span></a></li>
+        <li class="header">DOCUMENTOS</li>
+        <li><a href="form_bens_tae.php"><i class="glyphicon glyphicon-list-alt"></i> <span>Solicitação de Bens</span></a></li>
+    </ul>
+</section>
