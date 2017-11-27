572d4a5d04e8064fa56861a19670227bfa54f85b
lenonr <lenonrmsouza@gmail.com>
2017-11-27 18:52:57 -0200

    inserindo novas avaliacoes solicitacoes automaticamente

--- SAFD/php/form_bens_2_docente.php
+++ SAFD/php/form_bens_2_docente.php
@@ -3,30 +3,50 @@
     include 'conexao.php';
 
     # recebendo dados do formulario
-    $observacoes = $_POST['observacoes'];
-    $justificativa = $_POST['justificativa'];
-    $especificacoes = $_POST['especificacoes'];
-    $estrategia = $_POST['estrategia'];
-    $criterio = $_POST['criterio'];
-    $declaracao = $_POST['declaracao'];
-    $veracidade = $_POST['veracidade'];
+    // $observacoes = $_POST['observacoes'];
+    // $justificativa = $_POST['justificativa'];
+    // $especificacoes = $_POST['especificacoes'];
+    // $estrategia = $_POST['estrategia'];
+    // $criterio = $_POST['criterio'];
+    // $declaracao = $_POST['declaracao'];
+    // $veracidade = $_POST['veracidade'];
 
     # montando consulta SQL - ADICIONANDO OBJETO
-    // $sql = "INSERT INTO objeto(nome_objeto, especificacoestecnicas_objeto, estrategiafornecimento_objeto, preco_objeto, id_grupo)
+    # $sql = "INSERT INTO solicitacao(nome_objeto, especificacoestecnicas_objeto, estrategiafornecimento_objeto, preco_objeto, id_grupo)
             // VALUES('$item', '$especificacoes', '$estrategia', '$preco', 1)";
 
-    if (isset($observacoes) 
-        AND isset($justificativa) 
-        AND isset($especificacoes) 
-        AND isset($estrategia) 
-        AND isset($criterio) 
-        AND isset($declaracao) 
-        AND isset($veracidade)) 
-    {
-        // realizando inserção da tabela objeto
+    // # verificando se variaveis estao vazias
+    // if (isset($observacoes) 
+    //     AND isset($justificativa) 
+    //     AND isset($especificacoes) 
+    //     AND isset($estrategia) 
+    //     AND isset($criterio) 
+    //     AND isset($declaracao) 
+    //     AND isset($veracidade)) 
+    // {
+
+    # criando avaliacoes automaticamente
+    $sql = "INSERT INTO avaliacao_dad(recursos_avaliacaodad, comentarios_avaliacaodad. valorestimadodespesa_avaliacaodad, id_status, id_tipodespesa) 
+     		VALUES('vazio', 'vazio', 0, 1, 1);";
+
+    $sql .= "INSERT INTO avaliacao_dg(resposta_avaliacaodg, observacao_avaliacaodg, id_status)
+            VALUES('', '', 1);";
+
+    $sql .= "INSERT INTO avaliacao_dpdi(comentarios_avaliacaodpdi, planejamentoexercicio_avaliacaodpdi, id_status, id_unidadegestora)
+    		VALUES('', '', 1, 1);";
+
+	$sql .= "INSERT INTO avaliacao_coord(resposta_avaliacaocoord, observacao_avaliacaocoord, id_status)
+			VALUES('', '', 1);";
+
+    	echo $sql;
+
+        // realizando inserção nas tabelas
         if(mysqli_query($con, $sql))
         {
-            // // echo "funcionou!";
+        	# mostra mensagem e redireciona pagina
+            // echo ("<script>alert('Alguns dados dessa solicitacao foram adicionados automaticamente!'); location.href='form_bens_2_docente.php';</script>");
+
+            echo "funcionou!";
 
             // // // SEGUNDA INSERÇAO - ADICIONANDO AVALIACOES
             // // $sql = "INSERT INTO avaliacao_dad(recursos_avaliacaodad, comentarios_avaliacaodad, valorestimadodespesa_avaliacaodad, id_status, id_tipodespesa)    
@@ -41,142 +61,7 @@
             // # mostra mensagem e redireciona pagina
             // echo ("<script>alert('Objeto adicionado com sucesso!'); location.href='form_bens_obj_docente.php';</script>");
         }
-    }
+    // }
 ?>
 
-<!DOCTYPE html>
-<html>
-    <head>
-        <meta charset="utf-8">
-        <meta http-equiv="X-UA-Compatible" content="IE=edge">
-        <title>SAFD | Form. Bens </title>
-        <!-- Tell the browser to be responsive to screen width -->
-        <meta content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no" name="viewport">
-        <!-- Bootstrap 3.3.5 -->
-        <link rel="stylesheet" href="../bootstrap/css/bootstrap.min.css">
-        <!-- Font Awesome -->
-        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.4.0/css/font-awesome.min.css">
-        <!-- Ionicons -->
-        <link rel="stylesheet" href="https://code.ionicframework.com/ionicons/2.0.1/css/ionicons.min.css">
-        <!-- Theme style -->
-        <link rel="stylesheet" href="../dist/css/AdminLTE.min.css">
-        <!-- AdminLTE Skins. Choose a skin from the css/skins
-             folder instead of downloading all of them to reduce the load. -->
-        <link rel="stylesheet" href="../dist/css/skins/_all-skins.min.css">
-    </head>
-    <!--<body class="hold-transition skin-green fixed sidebar-mini">-->
-    <body class="hold-transition skin-green layout-fixed sidebar-mini">
-        <!-- Site wrapper -->
-        <div class="wrapper">
-            <header class="main-header" >
-                <?php
-                    include 'header_docente.php';
-                ?>
-            </header>
 
-            <!-- =============================================== -->
-
-            <!-- Left side column. contains the sidebar -->
-            <aside class="main-sidebar">
-                <?php
-                    include 'menulat_docente.php';
-                ?>
-            </aside>
-
-            <!-- =============================================== -->
-            <!--FORMULARIO DE BENS-->
-            <!--             <form onsubmit="alert('Solicitação enviada para avaliação do seu coordenador!')" action="system.php"> -->
-            <form action="gera_documento.php" method="post">
-                <div class="content-wrapper">
-                    <!-- MENSAGEM INICIAL -->
-                    <div class="alert alert-warning alert-dismissable">
-                        <button type="button" class="close" data-dismiss="alert"  aria-hidden="true">
-                            Fechar
-                        </button>
-                        <h4>
-                            <i class="icon fa fa-warning"> </i>
-                            Informações importantes
-                        </h4>
-                        <!--<label> - Alguns campos serão preenchidos automaticamente pelo sistema. </label>-->
-                        <!--<br>-->
-                        <label> - É necessário que todos os outros campos, sejam preenchidos! </label>
-                    </div>
-
-                    <section class="content-header">
-                        <h4>
-                            Solicitacao de Bens - Parte 2 - Descricao do Pedido;
-                        </h4>
-                        <ol class="breadcrumb">
-                            <li><a href=""><i class="fa fa-dashboard"></i> Inicio>Formulário para Solicitação de Bens </a></li>
-                        </ol>
-                    </section>                                                        
-
-                    <div class="col-xs-12">
-                        <br>
-                        <label>Observações</label>
-                        <textarea name="observacoes" class="form-control" required="" rows="2" placeholder="Solicitamos a/ao Ordenador(a) de Despesas autorização para instauração de procedimento licitatório para futura aquisição de material/contratação dos serviços(objeto, XXXXX) para (objetivo simplificado, XXXXX) conforme abaixo descritos." ></textarea>
-                    </div>                                 
-
-                    <div class="col-xs-12">
-                        <br>
-                        <label> Justificativa </label>
-                        <textarea name="justificativa" required="" class="form-control" rows="3" placeholder="Fundamentação bem elaborada da necessidade de compra, incluindo os motivos e os benefícios que se pretende alcançar com a aquisição." ></textarea>
-                    </div>
-
-                    <div class="col-xs-12">
-                        <br>
-                        <label> Especificações técnicas do objeto e local de entrega/necessidade e justificativa pra agrupamento de itens </label>
-                        <textarea name="especificacoes" required="" class="form-control" rows="5" placeholder="Indicar todos os requisitos desejados para o bem permanente ou material ed consumo que pretende adquirir. com descrições detalhadas, precisas e convincentes, incluindo as caracteristicas especificas. Indicar o(s) local(is) de entrega dos bens. Deverá ser indicado o endereço completo, bairro, CEP, inclusive número da sala ou prédio."></textarea>
-                    </div>
-
-                    <div class="col-xs-12">
-                        <br>
-                        <label>Estratégias de fornecimento, prazo de entrega ou prazo de execução.</label>
-                        <textarea name="estrategia" class="form-control" rows="5" required="" placeholder="Indicar o prazo da execução dos serviços e/ou prazo máximo de entrega dos materiais permanente/de consumo. Os materiais deverão ser entregues nos quantitativos e nas localidades indicadas acima no prazo máximo de 30 dias após a emissão do empenho. Os materiais deverão ter prazo de validade mínima de doze meses, contados a partir da data de entrega. Caso algum produto apresente defeito de fabricação quando em uso no decorrer do prazo de validade, o fornecedor deverá efetuar a troca do mesmo em cinco dias úteis, a contar da notificação, sem ônus adicional para o Instituto Federal Farroupilha."></textarea>
-                    </div>
-
-                    <div class="col-xs-12">
-                        <br>
-                        <label>Critérios de Aceitabilidade.</label>
-                        <textarea name="criterio" class="form-control" rows="8" required="" placeholder="Neste campo deverá ser informado de que maneira será realixado o recebimento provisório e o recebimento definitivo com o respectivo prazo. Exemplo 01: na aquisição de um eletroeletrônico o recebimento provisório poderá ser com a simples conferência física do aparelho e o recebimento definitivo, no prazo de XX dias a contar do recebimento provisório, com o teste a fim de verificar se o mesmo está funcionando corretamente. Exemplo 02: na aquisição de material ed consumo o recebimento provisório poderia ser com a conferência da quantidade solicitada e o recebimento definitivo, n no prazo de XX dias a contar do recebimento provisório, com a análise se todos os materiais estão em perfeitas condições de utilização."></textarea>
-                    </div>
-
-                    <div class="col-xs-12">
-                        <br>
-                        <label>Declaração de consulta ao saldo/estoque - Confirmação de Solicitação.</label>
-                        <textarea name="declaracao" class="form-control" rows="3" required="" placeholder="Declaro para fins de instauração licitatório, que consultei as áreas pertinentes a estoque e controle de saldo de materiais e serviços e obtive confirmação que os itens pretendidos não encontram-se disponíveis para retirada ou emissão de empenho."></textarea>
-                    </div>
-
-                    <div class="col-xs-12">
-                        <br>
-                        <label>Da veracidade dos orçamentos</label>
-                        <textarea name="veracidade" class="form-control" rows="3" required="" placeholder="Venho firmar que os orçamentos que compõe o preço médio acima estipulado, foram por mim realizados e são verdadeiros."></textarea>
-                    </div>
-
-                    <div class="box-footer">                        
-                        <button type="submit" class="btn btn-success pull-right">Enviar para Avaliação</button>
-                    </div>
-                </div>
-            </form>
-        <footer class="main-footer">
-            <?php
-                include 'footer.php';
-            ?>
-        </footer>
-
-        </div>
-
-        <!-- jQuery 2.1.4 -->
-        <script src="../plugins/jQuery/jQuery-2.1.4.min.js"></script>
-        <!-- Bootstrap 3.3.5 -->
-        <script src="../bootstrap/js/bootstrap.min.js"></script>
-        <!-- SlimScroll -->
-        <script src="../plugins/slimScroll/jquery.slimscroll.min.js"></script>
-        <!-- FastClick -->
-        <script src="../plugins/fastclick/fastclick.min.js"></script>
-        <!-- AdminLTE App -->
-        <script src="../dist/js/app.min.js"></script>
-        <!-- AdminLTE for demo purposes -->
-        <script src="../dist/js/demo.js"></script>
-    </body>
-</html>
