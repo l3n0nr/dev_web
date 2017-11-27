377d7ef5d119104d1b0548365ce499915f06e32f
lenonr <lenonrmsouza@gmail.com>
2017-11-27 19:48:59 -0200

    cadastrando solicitacao - verificar

--- SAFD/php/form_bens_2_docente.php
+++ SAFD/php/form_bens_2_docente.php
@@ -1,21 +1,18 @@
 <?php
     # incluindo arquivo de conexao
     include 'conexao.php';
+    include 'valida_sessao.php';
 
     # recebendo dados do formulario
-    // $observacoes = $_POST['observacoes'];
-    // $justificativa = $_POST['justificativa'];
-    // $especificacoes = $_POST['especificacoes'];
-    // $estrategia = $_POST['estrategia'];
-    // $criterio = $_POST['criterio'];
-    // $declaracao = $_POST['declaracao'];
-    // $veracidade = $_POST['veracidade'];
+    $observacoes = $_POST['observacoes'];
+    $justificativa = $_POST['justificativa'];
+    $especificacoes = $_POST['especificacoes'];
+    $estrategia = $_POST['estrategia'];
+    $criterio = $_POST['criterio'];
+    $declaracao = $_POST['declaracao'];
+    $veracidade = $_POST['veracidade'];    
 
-    # montando consulta SQL - ADICIONANDO OBJETO
-    # $sql = "INSERT INTO solicitacao(nome_objeto, especificacoestecnicas_objeto, estrategiafornecimento_objeto, preco_objeto, id_grupo)
-            // VALUES('$item', '$especificacoes', '$estrategia', '$preco', 1)";
-
-    // # verificando se variaveis estao vazias
+    # verificando se variaveis estao vazias
     // if (isset($observacoes) 
     //     AND isset($justificativa) 
     //     AND isset($especificacoes) 
@@ -24,62 +21,103 @@
     //     AND isset($declaracao) 
     //     AND isset($veracidade)) 
     // {
+	    # criando avaliacoes automaticamente   
+	    # avaliacao_dad
+	    $sql = "INSERT INTO avaliacao_dad(recursos_avaliacaodad, comentarios_avaliacaodad, valorestimadodespesa_avaliacaodad, id_status, id_tipodespesa)	
+	    		VALUES('', '', 0, 1, 1)";
 
-    # criando avaliacoes automaticamente   
-    # avaliacao_dad
-    $sql = "INSERT INTO avaliacao_dad(recursos_avaliacaodad, comentarios_avaliacaodad, valorestimadodespesa_avaliacaodad, id_status, id_tipodespesa)	
-    		VALUES('', '', 0, 1, 1)";
+	    if(mysqli_query($con, $sql))
+	    {
+	        // echo "teste 1";
+	    }
+	    else
+	    {
+	    	echo "Erro tabela avaliacao_dad";
+	    }
 
-    if(mysqli_query($con, $sql))
-    {
-        // echo "teste 1";
-    }
-    else
-    {
-    	echo "Erro tabela avaliacao_dad";
-    }
+		# avaliacao_dg
+	    $sql = "INSERT INTO avaliacao_dg(resposta_avaliacaodg, observacao_avaliacaodg, id_status)
+	            VALUES('', '', 1);";
 
-	# avaliacao_dg
-    $sql = "INSERT INTO avaliacao_dg(resposta_avaliacaodg, observacao_avaliacaodg, id_status)
-            VALUES('', '', 1);";
+	    if(mysqli_query($con, $sql))
+	    {
+	        // echo "teste 2";
+	    }
+	    else
+	    {
+	    	echo "Erro tabela avaliacao_dg";
+	    }
 
-    if(mysqli_query($con, $sql))
-    {
-        // echo "teste 2";
-    }
-    else
-    {
-    	echo "Erro tabela avaliacao_dg";
-    }
+	    # avaliacao_dpdi
+	    $sql = "INSERT INTO avaliacao_dpdi(comentarios_avaliacaodpdi, planejamentoexercicio_avaliacaodpdi, id_status, id_unidadegestora)
+	     		VALUES('', '', 1, 1)";
 
-    # avaliacao_dpdi
-    $sql = "INSERT INTO avaliacao_dpdi(comentarios_avaliacaodpdi, planejamentoexercicio_avaliacaodpdi, id_status, id_unidadegestora)
-     		VALUES('', '', 1, 1)";
+	    if(mysqli_query($con, $sql))
+	    {
+	        // echo "teste 3";
+	    }
+	    else
+	    {
+	    	echo "Erro tabela avaliacao_dpdi";
+	    }
 
-    if(mysqli_query($con, $sql))
-    {
-        // echo "teste 3";
-    }
-    else
-    {
-    	echo "Erro tabela avaliacao_dpdi";
-    }
+		# avaliacao_coord
+		$sql = "INSERT INTO avaliacao_coord(resposta_avaliacaocoord, observacao_avaliacaocoord, id_status)
+		 		VALUES('', '', 1);";
 
-	# avaliacao_coord
-	$sql = "INSERT INTO avaliacao_coord(resposta_avaliacaocoord, observacao_avaliacaocoord, id_status)
-	 		VALUES('', '', 1);";
+		if(mysqli_query($con, $sql))
+	    {
+	        // echo "teste 4";
+	    }
+		else
+	    {
+	    	echo "Erro tabela avaliacao_coord";
+	    }	
 
-	if(mysqli_query($con, $sql))
-    {
-        // echo "teste 4";
-    }
-	else
-    {
-    	echo "Erro tabela avaliacao_coord";
-    }	
+	    # consultando ultima avaliacao cadastrada 
+		$avaliacao = "SELECT id_avaliacaodad FROM avaliacao_dad ORDER BY id_avaliacaodad DESC limit 1";
 
+		// preparando variavel
+		$result = mysqli_query($con, $avaliacao);
 
+		// capturando valor funcao usuario
+		while($res = mysqli_fetch_assoc($result))
+		{
+    		$ultima_avaliacao = $res['id_avaliacaodad'];
+		}
+
+		echo $ultima_avaliacao;
+
+		$sessao = $_SESSION["usuario"];
+
+		# consultando usuario do sistema
+		$usuario = "SELECT id_usuario FROM usuario 
+					WHERE login_usuario = '$sessao';";
+
+		// echo $usuario;
+
+		// preparando variavel
+		$result = mysqli_query($con, $usuario);
+
+		// capturando valor funcao usuario
+		while($res = mysqli_fetch_assoc($result))
+		{
+		    $usuario_sessao = $res['id_usuario'];
+		}
+
+		echo $usuario_sessao;
+
+   		# montando consulta SQL - adicionando solicitacao - agora sim
+   		$sql = "INSERT INTO solicitacao(numeroprotocolo_solicitacao, justificativa_solicitacao, criterio_aceitabilidade, consulta_estoque, id_usuario, id_status, id_avaliacaodpdi, id_avaliacaodad, id_avaliacaocoord, id_avaliacaodg) 
+    			VALUES ("","","","", $usuario_sessao, 1, $ultima_avaliacao, $ultima_avaliacao, $ultima_avaliacao, $ultima_avaliacao);"
+
+   		echo $sql;
+    // }s
+    // else
+    // {
+    // 	echo "teste";
     // }
+
 ?>
 
 
