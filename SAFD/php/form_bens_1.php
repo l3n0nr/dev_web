b1e77983d3826cb9abd0ee51b4aec7ce33b1dce7
lenonr <lenonrmsouza@gmail.com>
2017-11-27 23:49:31 -0200

    arquivos separados para melhor controle, instruções SQL

---
+++ SAFD/php/form_bens_1.php
@@ -0,0 +1,33 @@
+<?php
+    # incluindo arquivo de conexao
+    include 'conexao.php';
+
+    # recebendo dados do formulario
+    $item = $_POST['item'];
+    $estrategia = $_POST['estrategia'];
+    $especificacoes = $_POST['especificacoes'];
+    $preco = $_POST['preco'];
+    $quantidade = $_POST['quantidade'];
+
+    # montando consulta SQL - ADICIONANDO OBJETO
+    $sql = "INSERT INTO objeto(nome_objeto, especificacoestecnicas_objeto, estrategiafornecimento_objeto, preco_objeto, id_grupo)
+            VALUES('$nome_objeto', '$especificacoes', '$estrategia', '$preco', 1)";
+
+    // realizando inserção da tabela objeto
+    if(mysqli_query($con, $sql))
+    {
+        // echo "funcionou!";
+
+        echo ("<script>alert('Objeto adicionado com sucesso!'); location.href='';</script>");
+
+        // SEGUNDA INSERÇAO - ADICIONANDO AVALIACOES
+        // $sql = "INSERT INTO avaliacao_dad(recursos_avaliacaodad, comentarios_avaliacaodad, valorestimadodespesa_avaliacaodad, id_status, id_tipodespesa)    
+        //         VALUES("", "", 0, 1, 1)";
+
+        // // realizando inserção na tabela - avaliacao dad 
+        // if(mysqli_query($con, $sql))
+        // {   
+        //     echo "teste";
+        // }
+    }
+?>
--- SAFD/php/form_bens_2.php
+++ SAFD/php/form_bens_2.php
@@ -1,222 +1,184 @@
-<!DOCTYPE html>
-<html>
-    <head>
-        <meta charset="utf-8">
-        <meta http-equiv="X-UA-Compatible" content="IE=edge">
-        <title>SAFD | Form. Bens </title>
-        <!-- Tell the browser to be responsive to screen width -->
-        <meta content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no" name="viewport">
-        <!-- Bootstrap 3.3.5 -->
-        <link rel="stylesheet" href="../bootstrap/css/bootstrap.min.css">
-        <!-- Font Awesome -->
-        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.4.0/css/font-awesome.min.css">
-        <!-- Ionicons -->
-        <link rel="stylesheet" href="https://code.ionicframework.com/ionicons/2.0.1/css/ionicons.min.css">
-        <!-- Theme style -->
-        <link rel="stylesheet" href="../dist/css/AdminLTE.min.css">
-        <!-- AdminLTE Skins. Choose a skin from the css/skins
-             folder instead of downloading all of them to reduce the load. -->
-        <link rel="stylesheet" href="../dist/css/skins/_all-skins.min.css">
-    </head>
-    <!--<body class="hold-transition skin-green fixed sidebar-mini">-->
-    <body class="hold-transition skin-green layout-fixed sidebar-mini">
-        <!-- Site wrapper -->
-        <div class="wrapper">
-            <header class="main-header" >
-                <?php
-                    include 'header_user.php';
-                ?>
-            </header>
+<?php
+    # incluindo arquivo de conexao
+    include 'conexao.php';
+    include 'valida_sessao.php';
 
-            <!-- =============================================== -->
+    # recebendo dados do formulario
+    $observacoes = $_POST['observacoes'];
+    $justificativa = $_POST['justificativa'];
+    $especificacoes = $_POST['especificacoes'];
+    $estrategia = $_POST['estrategia'];
+    $criterio = $_POST['criterio'];
+    $declaracao = $_POST['declaracao'];
+    $veracidade = $_POST['veracidade'];    
 
-            <!-- Left side column. contains the sidebar -->
-            <aside class="main-sidebar">
-                <?php
-                    include 'menulat_user.php';
-                ?>
-            </aside>
+    # verificando se variaveis estao vazias
+    if (isset($observacoes) 
+        AND isset($justificativa) 
+        AND isset($especificacoes) 
+        AND isset($estrategia) 
+        AND isset($criterio) 
+        AND isset($declaracao) 
+        AND isset($veracidade)) 
+    {
+        # criando avaliacoes automaticamente   
+        # avaliacao_dad
+        $sql = "INSERT INTO avaliacao_dad(recursos_avaliacaodad, comentarios_avaliacaodad, valorestimadodespesa_avaliacaodad, id_status, id_tipodespesa)    
+                VALUES('', '', 0, 1, 1)";
 
-            <!-- =============================================== -->
-            <!--FORMULARIO DE BENS-->
-            <!--             <form onsubmit="alert('Solicitação enviada para avaliação do seu coordenador!')" action="system.php"> -->
-            <form action="gera_documento.php" method="post">
-                <div class="content-wrapper">
-                    <!-- MENSAGEM INICIAL -->
-                    <div class="alert alert-warning alert-dismissable">
-                        <button type="button" class="close" data-dismiss="alert"  aria-hidden="true">
-                            Fechar
-                        </button>
-                        <h4>
-                            <i class="icon fa fa-warning"> </i>
-                            Informações importantes
-                        </h4>
-                        <!--<label> - Alguns campos serão preenchidos automaticamente pelo sistema. </label>-->
-                        <!--<br>-->
-                        <label> - É necessário que todos os outros campos, sejam preenchidos! </label>
-                    </div>
+        if(mysqli_query($con, $sql))
+        {
+            // echo "teste 1";
+        }
+        else
+        {
+            echo "Erro tabela avaliacao_dad";
+        }
 
-                    <section class="content-header">
-                        <h4>
-                            REQUISIÇÃO DE COMPRA - SOLICITAÇÃO DE BENS
-                        </h4>
-                        <ol class="breadcrumb">
-                            <li><a href=""><i class="fa fa-dashboard"></i> Inicio>Formulário para Solicitação de Bens </a></li>
-                        </ol>
-                    </section>                                                        
+        # avaliacao_dg
+        $sql = "INSERT INTO avaliacao_dg(resposta_avaliacaodg, observacao_avaliacaodg, id_status)
+                VALUES('', '', 1);";
 
-                    <div class="col-xs-12">
-                        <br>
-                        <label>Observações</label>
-                        <textarea name="observacoes" class="form-control" required="" rows="2" placeholder="Solicitamos a/ao Ordenador(a) de Despesas autorização para instauração de procedimento licitatório para futura aquisição de material/contratação dos serviços(objeto, XXXXX) para (objetivo simplificado, XXXXX) conforme abaixo descritos." ></textarea>
-                    </div>
+        if(mysqli_query($con, $sql))
+        {
+            // echo "teste 2";
+        }
+        else
+        {
+            echo "Erro tabela avaliacao_dg";
+        }
 
-                    <form action="form_bens.php" method="post">
-                        <hr>
-                        <!-- <div class="col-xs-2">
-                            <br>
-                            <label>Grupo </label>
-                            <select class="form-control" name="grupo" required="">
-                                <option value="1"> Tipo 1 </option>
-                                <option value="2"> Tipo 2 </option>
-                            </select>
-                        </div> -->
+        # avaliacao_dpdi
+        $sql = "INSERT INTO avaliacao_dpdi(comentarios_avaliacaodpdi, planejamentoexercicio_avaliacaodpdi, id_status, id_unidadegestora)
+                VALUES('', '', 1, 1)";
 
-                        <div class="col-xs-2">
-                            <br>
-                            <label>Item </label>
-                            <select class="form-control" name="item" required="">
-                                <option value="1"> Canetas </option>
-                                <option value="2"> Lapis </option>
-                            </select>
-                        </div>
+        if(mysqli_query($con, $sql))
+        {
+            // echo "teste 3";
+        }
+        else
+        {
+            echo "Erro tabela avaliacao_dpdi";
+        }
 
-                        <div class="col-xs-7">
-                            <br>
-                            <label>Especificações </label>
-                            <input name="especificacoes" class="form-control" type="text" required="" placeholder="autocompletar">
-                        </div>
+        # avaliacao_coord
+        $sql = "INSERT INTO avaliacao_coord(resposta_avaliacaocoord, observacao_avaliacaocoord, id_status)
+                VALUES('', '', 1);";
 
-                        <div class="col-xs-1">
-                            <br>
-                            <label>Quantidade</label>
-                            <input name="quantidade" class="form-control" type="number" required="" placeholder="0">
-                        </div>
+        if(mysqli_query($con, $sql))
+        {
+            // echo "teste 4";
+        }
+        else
+        {
+            echo "Erro tabela avaliacao_coord";
+        }   
 
-                        <div class="col-xs-2">
-                            <br>
-                            <br>
-                            <button type="submit" class="btn btn-sucess pull-right" required=""> Adicionar na lista </button>
-                        </div>
-                    </form>
+        # consultando ultima avaliacao cadastrada 
+        $avaliacao = "SELECT id_avaliacaodad FROM avaliacao_dad ORDER BY id_avaliacaodad DESC limit 1";
 
-                   <div class="col-xs-12">
-                        <div class="box-body">
-                            <div id="example2_wrapper" class="dataTables_wrapper form-inline dt-bootstrap">
-                                <div class="row">
-                                    <div class="col-sm-6"></div>
-                                    <div class="col-sm-3"></div>
-                                </div>
-                                <div class="row">
-                                    <div class="col-sm-12">
-                                        <table id="example2" class="table table-bordered table-hover dataTable" role="grid" aria-describedby="example2_info">
-                                            <thead>
-                                            <th class="sorting" tabindex="0" aria-controls="example2" rowspan="1" colspan="1" aria-label="">
-                                                Grupo
-                                            </th>
-                                            <th class="sorting" tabindex="0" aria-controls="example2" rowspan="1" colspan="1" aria-label="">
-                                                Item
-                                            </th>
-                                            <th class="sorting" tabindex="0" aria-controls="example2" rowspan="1" colspan="1" aria-label="">
-                                                Especificações
-                                            </th>
-                                            <th class="sorting" tabindex="0" aria-controls="example2" rowspan="1" colspan="1" aria-label="">
-                                                Quantidade
-                                            </th>
-                                            </thead>
-                                            <tbody>
-                                                <tr>
-                                                    <td>".."</td>
-                                                    <td>".."</td>
-                                                    <td>".."</td>
-                                                    <td>".."</td>
-                                                </tr>
-                                            </tbody>
-                                        </table>
-                                    </div>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
+        // preparando variavel
+        $result = mysqli_query($con, $avaliacao);
 
-                    <div class="col-xs-12">
-                        <br>
-                        <label> Justificativa </label>
-                        <textarea name="justificativa" required="" class="form-control" rows="3" placeholder="Fundamentação bem elaborada da necessidade de compra, incluindo os motivos e os benefícios que se pretende alcançar com a aquisição." ></textarea>
-                    </div>
+        // capturando valor funcao usuario
+        while($res = mysqli_fetch_assoc($result))
+        {
+            $ultima_avaliacao = $res['id_avaliacaodad'];
+        }
 
-                    <div class="col-xs-12">
-                        <br>
-                        <label> Especificações técnicas do objeto e local de entrega/necessidade e justificativa pra agrupamento de itens </label>
-                        <textarea name="especificacoes" required="" class="form-control" rows="5" placeholder="Indicar todos os requisitos desejados para o bem permanente ou material ed consumo que pretende adquirir. com descrições detalhadas, precisas e convincentes, incluindo as caracteristicas especificas. Indicar o(s) local(is) de entrega dos bens. Deverá ser indicado o endereço completo, bairro, CEP, inclusive número da sala ou prédio."></textarea>
-                    </div>
+        // echo $ultima_avaliacao;
 
-                    <div class="col-xs-12">
-                        <br>
-                        <label>Estratégias de fornecimento, prazo de entrega ou prazo de execução.</label>
-                        <textarea name="estrategia" class="form-control" rows="5" required="" placeholder="Indicar o prazo da execução dos serviços e/ou prazo máximo de entrega dos materiais permanente/de consumo. Os materiais deverão ser entregues nos quantitativos e nas localidades indicadas acima no prazo máximo de 30 dias após a emissão do empenho. Os materiais deverão ter prazo de validade mínima de doze meses, contados a partir da data de entrega. Caso algum produto apresente defeito de fabricação quando em uso no decorrer do prazo de validade, o fornecedor deverá efetuar a troca do mesmo em cinco dias úteis, a contar da notificação, sem ônus adicional para o Instituto Federal Farroupilha."></textarea>
-                    </div>
+        $sessao = $_SESSION["usuario"];
 
-                    <div class="col-xs-12">
-                        <br>
-                        <label>Critérios de Aceitabilidade.</label>
-                        <textarea name="criterio" class="form-control" rows="8" required="" placeholder="Neste campo deverá ser informado de que maneira será realixado o recebimento provisório e o recebimento definitivo com o respectivo prazo. Exemplo 01: na aquisição de um eletroeletrônico o recebimento provisório poderá ser com a simples conferência física do aparelho e o recebimento definitivo, no prazo de XX dias a contar do recebimento provisório, com o teste a fim de verificar se o mesmo está funcionando corretamente. Exemplo 02: na aquisição de material ed consumo o recebimento provisório poderia ser com a conferência da quantidade solicitada e o recebimento definitivo, n no prazo de XX dias a contar do recebimento provisório, com a análise se todos os materiais estão em perfeitas condições de utilização."></textarea>
-                    </div>
+        # consultando usuario do sistema
+        $usuario = "SELECT id_usuario FROM usuario 
+                    WHERE login_usuario = '$sessao';";
 
-                    <div class="col-xs-12">
-                        <br>
-                        <label>Declaração de consulta ao saldo/estoque - Confirmação de Solicitação.</label>
-                        <textarea name="declaracao" class="form-control" rows="3" required="" placeholder="Declaro para fins de instauração licitatório, que consultei as áreas pertinentes a estoque e controle de saldo de materiais e serviços e obtive confirmação que os itens pretendidos não encontram-se disponíveis para retirada ou emissão de empenho."></textarea>
-                    </div>
+        // echo $usuario;
 
-                    <div class="col-xs-12">
-                        <br>
-                        <label>Da veracidade dos orçamentos</label>
-                        <textarea name="veracidade" class="form-control" rows="3" required="" placeholder="Venho firmar que os orçamentos que compõe o preço médio acima estipulado, foram por mim realizados e são verdadeiros."></textarea>
-                    </div>
+        // preparando variavel
+        $result = mysqli_query($con, $usuario);
 
-                    <div class="box-footer">
-                        <button type="submit" class="btn btn-success pull-right">Enviar para Avaliação</button>
-                    </div>
-                </div>
-            </form>
+        // capturando valor funcao usuario
+        while($res = mysqli_fetch_assoc($result))
+        {
+            $usuario_sessao = $res['id_usuario'];
+        }
 
+        // echo $usuario_sessao;
 
-            <footer class="main-footer">
-                <?php
-                include 'footer.php';
-                ?>
-            </footer>
+        # montando instrucao SQL - adicionando solicitacao - agora sim
+        $sql = "INSERT INTO solicitacao(numeroprotocolo_solicitacao, justificativa_solicitacao, criterio_aceitabilidade, consulta_estoque, id_usuario, id_status, id_avaliacaodpdi, id_avaliacaodad, id_avaliacaocoord, id_avaliacaodg) 
+                VALUES ('00000','$justificativa','$criterio','$declaracao', '$usuario_sessao', 1, '$ultima_avaliacao', '$ultima_avaliacao', '$ultima_avaliacao', '$ultima_avaliacao');";
 
-            <!-- Stats tab content -->
-        </form>
-    </div><!-- /.tab-pane -->
-</div>
-</aside><!-- /.control-sidebar -->
+        // echo $sql;
 
-</div><!-- ./wrapper -->
+        # cadastrando nova solicitacao
+        if(mysqli_query($con, $sql))
+        {
+            // echo "Solicitacao cadastrada com sucesso!";
+        }
+        else
+        {
+            echo "Erro tabela avaliacao_coord";
+        }    
 
-<!-- jQuery 2.1.4 -->
-<script src="../plugins/jQuery/jQuery-2.1.4.min.js"></script>
-<!-- Bootstrap 3.3.5 -->
-<script src="../bootstrap/js/bootstrap.min.js"></script>
-<!-- SlimScroll -->
-<script src="../plugins/slimScroll/jquery.slimscroll.min.js"></script>
-<!-- FastClick -->
-<script src="../plugins/fastclick/fastclick.min.js"></script>
-<!-- AdminLTE App -->
-<script src="../dist/js/app.min.js"></script>
-<!-- AdminLTE for demo purposes -->
-<script src="../dist/js/demo.js"></script>
-</body>
-</html>
+        # consultando ultima solicitacao cadastrada 
+        $solicitacao = "SELECT id_solicitacao FROM solicitacao ORDER BY id_solicitacao DESC limit 1";
+
+        // preparando variavel
+        $result = mysqli_query($con, $solicitacao);
+
+        // capturando valor funcao usuario
+        while($res = mysqli_fetch_assoc($result))
+        {
+            $ultima_solicitacao = $res['id_solicitacao'];
+        }
+
+        // echo $ultima_solicitacao;     
+
+        # consultando objetos cadastrados pelo usuario X[VERIFICAR]  
+        # existem duas opções possiveis
+        # ou, cria-se um campo dentro de tabela com o id_usuario para fazer uma referencia
+        # ou, cadastra-se apenas o ultimo objeto listado
+
+        $objeto = "SELECT id_objeto FROM objeto ORDER BY id_objeto DESC limit 1";
+
+        // preparando variavel
+        $result = mysqli_query($con, $objeto);
+
+        // capturando valor objeto
+        while($res = mysqli_fetch_assoc($result))
+        {
+            $ultimo_objeto = $res['id_objeto'];
+        }
+
+        // echo $ultimo_objeto;     
+
+        # montando instrucao SQL - solicitacao itens
+        $sql = "INSERT INTO solicitacao_itens(quantidade_objeto, prazoentrega_objeto, prazoexecucao_objeto, id_solicitacao, id_objeto) 
+                VALUES ('0','','', $ultima_solicitacao, '$ultimo_objeto');";
+
+        // echo $sql;
+
+        // # cadastrando nova solicitacao-item
+        if(mysqli_query($con, $sql))
+        {
+            // echo "Solicitacao cadastrada com sucesso!";
+
+            # mostra mensagem e redireciona pagina
+            // echo ("<script>alert('Solicitacao adicionada com sucesso!'); location.href='form_bens_2_docente.php';</script>");
+
+            echo ("<script>alert('Solicitacao adicionada com sucesso!'); location.href='';</script>");
+        }
+        else
+        {
+            echo "Erro tabela avaliacao_coord";
+        }  
+    }
+    else
+    {
+        // echo "teste";
+    }
+
+?>
