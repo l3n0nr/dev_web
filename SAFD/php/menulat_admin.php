725b95ced3d0ef08fb5e297319d627dbf5fa08fe
lenonr <lenonrmsouza@gmail.com>
2017-12-02 11:08:21 -0200

    arquivo melhorado - menu reconfigurado

--- SAFD/php/menulat_admin.php
+++ SAFD/php/menulat_admin.php
@@ -1,14 +1,28 @@
 <section class="sidebar">
-    <ul class="sidebar-menu">
-        <li class="header">USUÁRIOS</li>
-        <li><a href="p_insere_usuarios.php"><i class="glyphicon glyphicon-plus"></i> <span>Inserir usuários</span></a></li>
-        <li><a href="p_visualiza_usuarios.php"><i class="glyphicon glyphicon-sunglasses"></i> <span>Visualizar usuários</span></a></li>
-        <li><a href="p_edita_usuarios.php"><i class="glyphicon glyphicon-list-alt"></i> <span>Alterar usuários</span></a></li>
-        <li><a href="p_exclui_usuarios.php"><i class="glyphicon glyphicon-trash"></i> <span>Excluir usuário</span></a></li>
-        <li class="header">SOLICITAÇÃO</li>
-        <li><a href="form_bens_1_admin.php"><i class="glyphicon glyphicon-eye-open"></i> <span>
-        Visualiza Solicitações</span></a></li>
-        <li><a href=""><i class="glyphicon glyphicon-th"></i> <span>
-        Adiciona Objetos</span></a></li>
+    <ul class="sidebar-menu">               
+        <li class="treeview active menu-open">
+          <a href="#">
+            <i class="glyphicon glyphicon-user"></i>
+            <span>Usuarios</span>
+          </a>
+          <ul class="treeview-menu">
+            <li><a href="p_insere_usuarios.php"><i class="glyphicon glyphicon-plus"></i> <span>Inserir usuários</span></a></li>
+            <li><a href="p_visualiza_usuarios.php"><i class="glyphicon glyphicon-sunglasses"></i> <span>Visualizar usuários</span></a></li>
+            <li><a href="p_edita_usuarios.php"><i class="glyphicon glyphicon-list-alt"></i> <span>Alterar usuários</span></a></li>
+            <li><a href="p_exclui_usuarios.php"><i class="glyphicon glyphicon-trash"></i> <span>Excluir usuário</span></a></li>
+          </ul>
+        </li>
+
+        <li class="treeview active menu-open">
+          <a href="#">
+            <i class="glyphicon glyphicon-list-alt"></i>
+            <span>Solicitacao</span>
+          </a>
+          <ul class="treeview-menu">
+            <li><a href="form_bens_1_admin.php"><i class="glyphicon glyphicon-th"></i> <span> Adiciona Objetos</span></a></li>
+            <li><a href=""><i class="glyphicon glyphicon-eye-open"></i> <span> Visualiza Solicitações</span></a></li>            
+          </ul>
+
+        </li>
     </ul>
 </section>
