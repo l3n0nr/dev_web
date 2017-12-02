c2e498597f1086f73b9ea2ff834074685e81d0e6
lenonr <lenonrmsouza@gmail.com>
2017-12-02 10:50:14 -0200

    arquivos administrar melhorados

--- SAFD/php/menulat_admin.php
+++ SAFD/php/menulat_admin.php
@@ -5,5 +5,10 @@
         <li><a href="p_visualiza_usuarios.php"><i class="glyphicon glyphicon-sunglasses"></i> <span>Visualizar usuários</span></a></li>
         <li><a href="p_edita_usuarios.php"><i class="glyphicon glyphicon-list-alt"></i> <span>Alterar usuários</span></a></li>
         <li><a href="p_exclui_usuarios.php"><i class="glyphicon glyphicon-trash"></i> <span>Excluir usuário</span></a></li>
+        <li class="header">SOLICITAÇÃO</li>
+        <li><a href="form_bens_1_admin.php"><i class="glyphicon glyphicon-eye-open"></i> <span>
+        Visualiza Solicitações</span></a></li>
+        <li><a href=""><i class="glyphicon glyphicon-th"></i> <span>
+        Adiciona Objetos</span></a></li>
     </ul>
 </section>
