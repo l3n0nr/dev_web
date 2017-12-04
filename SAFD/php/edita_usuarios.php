1faa7bee5c814c1e7a11fb63bb4f5cc569722490
lenonr <lenonrmsouza@gmail.com>
2017-12-04 10:55:37 -0200

    tentando mostrar componentes tabela

--- SAFD/php/edita_usuarios.php
+++ SAFD/php/edita_usuarios.php
@@ -3,29 +3,15 @@
 //incluindo arquivo conexao
 include 'conexao.php';
 
-$sql = "SELECT login_usuario, estado_usuario, nome_setor, descricao_funcao, siape_funcionario, data_cadastro
-        FROM usuario, setor, funcao, funcionario
-        WHERE usuario.id_setor = setor.id_setor
-        AND usuario.id_funcao = funcao.id_funcao
-        AND funcionario.id_funcionario = usuario.id_funcionario";
+    $teste = 
+    "<tr>
+        <td> <input name="categoria" type="text" size="50" id="categoria" maxlength="100" value=""> </td>"
+            . "
+        <td>" .  . "</td>" . "
+        <td>" .  . "</td>" . "
+        <td>" .  . "</td>" . "
+        <td>" .  . "</td>" . "
+    </tr>";
 
-$result = mysqli_query($con, $sql);
-
-
-//criando repeticao para percorrer a tabela
-while ($linha = mysqli_fetch_assoc($result))
-{
-    #criando variavel para mostra colunas/linhas
-    $visualizar = "<tr>
-                        <td>" . $linha['siape_funcionario'] . "</td>" . "
-                        <td>" . $linha['login_usuario'] . "</td>" . "
-                        <td>" . $linha['nome_setor'] . "</td>" . "
-                        <td>" . $linha['descricao_funcao'] . "</td>" . "
-                        <td>" . $linha['data_cadastro'] . "</td>" . "
-                        <td> Editar </td>" . "
-                    </tr>";
-
-    #mostrando colunas/linhas
-    echo $visualizar;
-}
+    echo $teste;
 ?>
