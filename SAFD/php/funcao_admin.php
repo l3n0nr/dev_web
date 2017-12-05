c9542bf270c1c19be2308ea2abf69b4018934aca
lenonr <lenonrmsouza@gmail.com>
2017-12-05 11:56:11 -0200

    funcao administrador adicionada

--- SAFD/php/funcao_admin.php
+++ SAFD/php/funcao_admin.php
@@ -1,4 +1,5 @@
 <option value="0"> Função </option>
+<option value="1"> Administrador </option>
 <option value="2"> Diretor </option>
 <option value="3"> Coordenador </option>
 <option value="4"> TAE </option>
